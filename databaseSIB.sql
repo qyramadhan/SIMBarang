0f6b7bd9098c0d420586fef47a446c9b57681a12
rizqyramadhan <rizqyq9@gmail.com>
2024-03-22 09:22:40 +0700

    Updated Database

--- databaseSIB.sql
+++ databaseSIB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 07, 2024 at 08:29 AM
+-- Generation Time: Mar 22, 2024 at 03:20 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.3.22
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `role`
+-- Database: `databasesib`
 --
 
 -- --------------------------------------------------------
@@ -56,7 +56,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2019_08_19_000000_create_failed_jobs_table', 1),
 (3, '2024_02_02_014045_create_permission_tables', 1),
-(4, '2024_02_02_014343_create_products_table', 2);
+(4, '2024_02_02_014343_create_products_table', 2),
+(5, '2014_10_12_100000_create_password_resets_table', 3);
 
 -- --------------------------------------------------------
 
@@ -93,6 +94,18 @@ INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`) VALUES
 (3, 'App\\User', 13),
 (3, 'App\\User', 14),
 (3, 'App\\User', 15);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -245,8 +258,9 @@ CREATE TABLE `tb_barang` (
 --
 
 INSERT INTO `tb_barang` (`id_barang`, `nama_barang`, `id_kategori`, `tgl_pembelian`, `created_at`, `updated_at`, `soft_delete`, `log_user1`, `log_user2`, `last_action`) VALUES
-(1, 'Kursi Lipat', 1, '2024-03-01', '2024-02-28 19:19:20', '2024-02-28 19:35:01', 0, 1, 1, 2),
-(2, 'Kursi Duduk', 1, '2024-03-02', '2024-02-28 19:27:52', '2024-02-28 19:32:27', 1, 1, 1, 3);
+(1, 'Kursi Lipat', 1, '2024-03-17', '2024-02-28 19:19:20', '2024-03-21 03:37:50', 0, 1, 1, 2),
+(2, 'Kursi Duduk', 1, '2024-03-02', '2024-02-28 19:27:52', '2024-03-21 03:42:49', 0, 1, 1, 2),
+(3, 'Jam Dinding', 2, '2024-03-21', '2024-03-20 03:51:41', '2024-03-21 03:53:56', 0, 1, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -292,7 +306,7 @@ CREATE TABLE `tb_gedung` (
 --
 
 INSERT INTO `tb_gedung` (`id_gedung`, `kode_gedung`, `nama_gedung`, `created_at`, `updated_at`, `log_user1`, `log_user2`, `soft_delete`, `last_action`) VALUES
-(1, '01', 'Yayasan', '2024-02-20 01:48:49', '2024-02-22 06:19:15', 1, 1, 0, 1),
+(1, '01', 'Yayasan', '2024-02-20 01:48:49', '2024-03-20 07:52:07', 1, 1, 0, 2),
 (2, '02', 'Utama', '2024-02-20 01:48:49', '2024-02-22 06:19:15', 1, 1, 0, 1),
 (3, '03', 'Gedung Lainnya', '2024-02-27 00:08:14', '2024-02-27 00:44:40', 1, 1, 1, 3),
 (4, '03', 'Lain', '2024-02-29 00:01:27', '2024-02-29 00:01:34', 1, 1, 0, 2);
@@ -321,7 +335,7 @@ CREATE TABLE `tb_golongan` (
 
 INSERT INTO `tb_golongan` (`id_golongan`, `kode_golongan`, `nama_golongan`, `created_at`, `updated_at`, `soft_delete`, `log_user1`, `log_user2`, `last_action`) VALUES
 (1, '01', 'Golongan 1', '2024-02-20 21:13:35', '2024-02-20 21:13:35', 0, 1, 1, 1),
-(2, '02', 'Golongan 2', '2024-02-27 00:59:53', '2024-02-27 01:01:07', 1, 1, 1, 3);
+(2, '02', 'Golongan 2', '2024-02-27 00:59:53', '2024-03-20 03:39:26', 0, 1, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -347,10 +361,10 @@ CREATE TABLE `tb_jenis` (
 --
 
 INSERT INTO `tb_jenis` (`id_jenis`, `kode_jenis`, `nama_jenis`, `id_golongan`, `created_at`, `updated_at`, `soft_delete`, `log_user1`, `log_user2`, `last_action`) VALUES
-(1, '001', 'Kursi Kantor', 1, '2024-02-21 20:33:22', '2024-02-29 00:10:00', 0, 1, 1, 2),
+(1, '001', 'Kursi Kantor', 1, '2024-02-21 20:33:22', '2024-03-20 07:18:35', 0, 1, 1, 2),
 (2, '002', 'Kursi Kantor', 1, '2024-02-21 20:38:14', '2024-02-27 01:09:29', 1, 1, 1, 3),
-(3, '003', 'Meja Kantor', 1, '2024-02-21 20:38:26', '2024-02-27 02:35:04', 0, 1, 1, 1),
-(4, '004', 'Monitor Komputer', 1, '2024-02-21 20:39:18', '2024-02-27 02:35:04', 0, 1, 1, 1),
+(3, '003', 'Meja Kantor', 1, '2024-02-21 20:38:26', '2024-03-20 07:17:36', 0, 1, 1, 2),
+(4, '004', 'Monitor Komputer', 2, '2024-02-21 20:39:18', '2024-03-20 03:39:41', 0, 1, 1, 2),
 (5, '005', 'PC Lab', 1, '2024-02-21 20:44:38', '2024-02-27 02:35:04', 0, 1, 1, 1),
 (6, '006', 'Printer Lab', 1, '2024-02-21 21:10:14', '2024-02-27 01:07:45', 0, 1, 1, 2);
 
@@ -377,7 +391,8 @@ CREATE TABLE `tb_kartubarang` (
 --
 
 INSERT INTO `tb_kartubarang` (`id_kartu`, `id_ruang`, `keterangan`, `created_at`, `updated_at`, `soft_delete`, `log_user1`, `log_user2`, `last_action`) VALUES
-(1, 1, 'Ada', '2024-02-29 00:17:00', '2024-02-29 07:51:34', 0, 1, 1, 2);
+(1, 1, 'Ada', '2024-02-29 00:17:00', '2024-02-29 07:51:34', 0, 1, 1, 2),
+(2, 2, 'Ada', '2024-03-20 04:19:31', '2024-03-20 04:19:31', 0, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -430,9 +445,9 @@ CREATE TABLE `tb_lantai` (
 --
 
 INSERT INTO `tb_lantai` (`id_lantai`, `kode_lantai`, `nama_lantai`, `id_gedung`, `created_at`, `updated_at`, `soft_delete`, `log_user1`, `log_user2`, `last_action`) VALUES
-(1, '001', 'Lantai 1', 2, '2024-02-20 19:55:58', '2024-02-29 07:03:52', 0, 1, 1, 2),
+(1, '001', 'Lantai 1', 2, '2024-02-20 19:55:58', '2024-03-21 07:41:39', 0, 1, 1, 2),
 (2, '002', 'Lantai 2', 2, '2024-02-20 19:56:20', '2024-02-27 00:53:37', 1, 1, 1, 3),
-(3, '002', 'Lantai 2', 4, '2024-02-28 23:53:41', '2024-02-29 00:09:03', 0, 1, 1, 2);
+(3, '002', 'Lantai 2', 4, '2024-02-28 23:53:41', '2024-03-21 06:28:22', 0, 1, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -540,6 +555,7 @@ CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -551,9 +567,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Hardik Savani', 'admin@gmail.com', NULL, '$2y$10$OLAphs7tS9F0zXbe1.HyROnl1ctNLbI5WrZfTxvRvfIsUkOP99N.K', 'zxhsS8pNLOe7f9BqdF2CMtXwTq8uB86uElPtIRCjlvOAKuD7BavnEafJF3A0', '2024-02-01 19:34:28', '2024-02-01 19:34:28'),
-(15, 'Mainaky', 'main@gmail.com', NULL, '$2y$10$0lbbjLjhdkrpOvxD/xCIg.RB2qAe/RNUfEgMb42ZdV.Q0tlebE756', NULL, '2024-02-15 00:43:34', '2024-02-15 23:30:01');
+INSERT INTO `users` (`id`, `name`, `email`, `username`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Hardik Savani', 'admin@gmail.com', 'admin', NULL, '$2y$10$OLAphs7tS9F0zXbe1.HyROnl1ctNLbI5WrZfTxvRvfIsUkOP99N.K', 'X9jiWjPs1H9zWQ2rmUcxySUC7AFejXv9ogveep4VlcTgquP1tlULDoHdWFIl', '2024-02-01 19:34:28', '2024-02-01 19:34:28'),
+(15, 'Mainaky', 'main@gmail.com', 'user', NULL, '$2y$10$0lbbjLjhdkrpOvxD/xCIg.RB2qAe/RNUfEgMb42ZdV.Q0tlebE756', NULL, '2024-02-15 00:43:34', '2024-02-15 23:30:01');
 
 --
 -- Indexes for dumped tables
@@ -584,6 +600,12 @@ ALTER TABLE `model_has_permissions`
 ALTER TABLE `model_has_roles`
   ADD PRIMARY KEY (`role_id`,`model_id`,`model_type`),
   ADD KEY `model_has_roles_model_id_model_type_index` (`model_id`,`model_type`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
 
 --
 -- Indexes for table `permissions`
@@ -711,7 +733,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `permissions`
@@ -741,7 +763,7 @@ ALTER TABLE `tb_action`
 -- AUTO_INCREMENT for table `tb_barang`
 --
 ALTER TABLE `tb_barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tb_detailkartu`
@@ -771,7 +793,7 @@ ALTER TABLE `tb_jenis`
 -- AUTO_INCREMENT for table `tb_kartubarang`
 --
 ALTER TABLE `tb_kartubarang`
-  MODIFY `id_kartu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_kartu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tb_kategori`
